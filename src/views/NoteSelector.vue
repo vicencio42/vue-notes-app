60d901341ee1fdb3f658b4114aec92ab4508da8f
Vicencio42 <vicencio42@gmail.com>
2022-04-13 21:22:19 -0500

    componentized the selector/s added dynamic notes with props

---
+++ src/views/NoteSelector.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="note-selector">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["note"],
+};
+</script>
--- src/views/NoteSelectors.vue
+++ src/views/NoteSelectors.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="note-selectors">
-    <div class="note-selector active">
-      <p class="note-selector-title">First note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Second note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Third note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
+    <NoteSelector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" />
   </div>
 </template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+      ],
+    };
+  },
+};
+</script>
