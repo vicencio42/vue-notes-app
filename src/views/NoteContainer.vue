b44e3fd249526c61fc50f91d282d8cb9d3e14173
Vicencio42 <vicencio42@gmail.com>
2022-04-13 20:44:31 -0500

    componentized the editor

--- src/views/NoteContainer.vue
+++ src/views/NoteContainer.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="note-container">
     <NoteSelectors />
-    <div class="note-editor">
-      <p class="note-editor-info">Timestamp here...</p>
-      <textarea class="note-editor-input">
-          First note...
-
-          Note text here...
-        </textarea
-      >
-    </div>
+    <NoteEditor />
   </div>
 </template>
 
 <script>
 import NoteSelectors from "./NoteSelectors.vue";
+import NoteEditor from "./NoteEditor.vue";
 
 export default {
   components: {
     NoteSelectors,
+    NoteEditor,
   },
 };
 </script>
---
+++ src/views/NoteEditor.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="note-editor">
+    <p class="note-editor-info">Timestamp here...</p>
+    <textarea class="note-editor-input">
+          First note...
+
+          Note text here...
+        </textarea
+    >
+  </div>
+</template>
