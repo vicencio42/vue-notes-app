bbe3e8e63550aecffabac7e90bd5c8f5120cad77
Vicencio42 <vicencio42@gmail.com>
2022-04-13 20:32:02 -0500

    componentized the container

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,18 @@
 <template>
   <div id="app">
     <NoteToolbar />
-    <div class="note-container">
-      <div class="note-selectors">
-        <div class="note-selector active">
-          <p class="note-selector-title">First note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Second note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Third note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-      </div>
-      <div class="note-editor">
-        <p class="note-editor-info">Timestamp here...</p>
-        <textarea class="note-editor-input">
-          First note...
-
-          Note text here...
-        </textarea>
-      </div>
-    </div>
+    <NoteContainer />
   </div>
 </template>
 
 <script>
 import NoteToolbar from "./views/NoteToolbar.vue";
+import NoteContainer from "./views/NoteContainer.vue";
 
 export default {
   components: {
     NoteToolbar,
+    NoteContainer,
   },
   data: function () {
     return {};
